16894e340eb5c6118381a19a80124865dc2ab562
iineolineii <leonidsem10@gmail.com>
2026-03-28 17:19:23 +0300

    Start implementing main entry point

--- hi3-avatar-sorter/__main__.py
+++ hi3-avatar-sorter/__main__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-from typing import Literal
 
 from . import PART1_VALKYRIES, PART2_VALKYRIES
+from .errors import ParsingError
 from .models import Avatar
 from .models.part import build_valkyrie_db
 from .utils import validate_paths
@@ -27,42 +27,40 @@ RawAvatar = tuple[int | str, int | str, int | str]
 AvatarTuple = RawAvatar | RawAvatarWithNote | RawAvatarWithSkin | RawAvatarWithSkinAndNote
 
 replacement_table: dict[str | AvatarTuple | Avatar, str | AvatarTuple | Avatar] = {
+    # Beach avatars do not have battlesuit IDs
+    (PART1, KIANA,   "", 4, 1): (PART1, KIANA,   1, 4, 1),
+    (PART1, KIANA,   "", 4, 1): (PART1, KIANA,   1, 4, 1),
+    (PART1, MEI,     "", 4, 1): (PART1, MEI,     1, 4, 1),
+    (PART1, MEI,     "", 4, 1): (PART1, MEI,     1, 4, 1),
+    (PART1, BRONYA,  "", 4, 1): (PART1, BRONYA,  1, 4, 1),
+    (PART1, BRONYA,  "", 4, 1): (PART1, BRONYA,  1, 4, 1),
+    (PART1, THERESA, "", 4, 1): (PART1, THERESA, 1, 4, 1),
+    (PART1, THERESA, "", 4, 1): (PART1, THERESA, 1, 4, 1),
+    (PART1, HIMEKO,  "", 4, 1): (PART1, HIMEKO,  1, 4, 1),
+    (PART1, HIMEKO,  "", 4, 1): (PART1, HIMEKO,  1, 4, 1),
+
+    # This avatar's battlesuit ID is 1 character long instead of 2
+    (PART1, SAKURA, 1, 4, 1): (PART1, SAKURA, 1, 4, 1),
+
+    # This avatar's Valkyrie ID is 1 character long instead of 2
+    (PART1, KIANA, 15, 4, 1): (PART1, KIANA, 15, 4, 1),
+
+    # HoV is 5th Kiana, not 3rd Kallen
+    (PART1, KALLEN, 13): (PART1, KIANA, 5),
+
     # 3rd Elysia battlesuit is not a new character
     (PART2, ELYSIA_P2, 1): (PART1, ELYSIA_P1, 3),
 
-    #
+    # This avatar has wrong battlesuit ID
     (PART1, RITA, 1, "Special"): (PART1, RITA, 3, "Special"),
 
-    # Beach skins
-    "601_04_01": (PART1, KIANA, 1, 4, 1),
-
-    "602_04_01": (PART1, MEI, 1, 4, 1),
-
-    # (PART1, BRONYA, 1, 4, 1): (PART1, BRONYA, 1, 4, 2),
-    "603_04_01": (PART1, BRONYA, 1, 4, 1),
-
-    # (PART1, THERESA, 1, 4, 1): (PART1, THERESA, 1, 4, 2),
-    "604_04_01": (PART1, THERESA, 1, 4, 1),
-
-    # (PART1, HIMEKO, 1, 4, 2): (PART1, HIMEKO, 1, 4, 3),
-    # (PART1, HIMEKO, 1, 4, 1): (PART1, HIMEKO, 1, 4, 2),
-    "605_04_01": (PART1, HIMEKO, 1, 4, 1),
-
-    "6021_04_01": (PART1, SAKURA, 1, 4, 1),
-
-    "6115_04_01": (PART1, KIANA, 15, 4, 1),
-
-    # HoV is 5th Kiana, not 3rd Kallen
-    # (PART1, KIANA, 6) : (PART1, KIANA, 7),
-    # (PART1, KIANA, 5) : (PART1, KIANA, 6),
-    (PART1, KALLEN, 13): (PART1, KIANA, 5),
-
     # NOTE#1:
-    # Kallen battlesuits were shifted to the 50s range to
+    # Because Kallen and Kiana are completely messed up,
+    # Kallen's battlesuits were shifted to the 50s range to
     # free some ID space for future Kiana battlesuits
     #
-    # IMPORTANT: Kiana's max battlesuit ID must also be
-    # set to 50 in the Valkyrie DB
+    # IMPORTANT: For this fix to work, Kiana's max
+    # battlesuit ID must also be set to 50 in the Valkyrie DB
     #
     # EDGE CASE: Unrealistically to happen, but in case of
     # a new Kallen battlesuit, the below list should be
@@ -75,12 +73,23 @@ replacement_table: dict[str | AvatarTuple | Avatar, str | AvatarTuple | Avatar] 
 
 def main(
     source_folder: str | Path,
-    output_folder: str | Path = "output",
-    format: Literal["short", "long"] = "long"
+    output_folder: str | Path = "output"
 ):
     source_folder, output_folder = validate_paths(source_folder, output_folder)
     build_valkyrie_db(PART1_VALKYRIES, PART2_VALKYRIES)
 
-    for file in source_folder.iterdir():
-        # _, avatar_raw = Avatar._raw_from_file(file, format)
-        avatar = Avatar.from_file(file)
+    for file in sorted(source_folder.iterdir()):
+        if not file.is_file():
+            continue
+
+        try:
+            avatar = Avatar.from_string(file.stem)
+        except ParsingError as e:
+            print(f"\033[7m[SKIP]\033[0m {file.stem}: {str(e)}")
+        else:
+            print(f"{file.stem}:")
+            print(avatar)
+
+
+if __name__ == "__main__":
+    main(input("Enter source folder path: "))
