044cce5da557078ae7f4168200656e9f58004f41
iineolineii <leonidsem10@gmail.com>
2026-06-07 15:41:33 +0300

    Expand string utilities
    * hi3-avatar-sorter/utils/string.py:
      - Add the `snake_case` function.
      - Add the `capitalize` function.
      - Add the `_compute_stats`, `print_node`, and `tree` functions for domain model visualization.
      - Rename `build_fixers` to `build_fixers_map`. (Fixes #5)
      - Add the `misspelled note` fixer to the builder.
      - Rename the `wrong ID` fixer to `wrong Battlesuit ID`
      - Switch the `wrong Battlesuit ID` fixer class to the `PrefixFixer`.
      - Update `fix_avatar_string` to accept a dictionary of fixers.
      - Update `fix_avatar_string` to return the fix name alongside the fixed string.

--- hi3-avatar-sorter/utils/__init__.py
+++ hi3-avatar-sorter/utils/__init__.py
@@ -1,14 +1,21 @@
 from .container import FrozenMultiDict, MexContainer, mex_field
-from .path import get_format_by_folder, validate_and_sort_files, validate_paths
-from .string import fix_avatar_string
+from .model import DataclassEncoder, build_avatars_map, build_raw_avatars_map
+from .path import get_format_by_folder, validate_paths
+from .string import build_fixers_map, capitalize, fix_avatar_string, snake_case, tree
 
 
 __all__ = [
     "FrozenMultiDict",
     "MexContainer",
     "mex_field",
+    "DataclassEncoder",
+    "build_avatars_map",
+    "build_raw_avatars_map",
     "get_format_by_folder",
     "validate_paths",
-    "validate_and_sort_files",
-    "fix_avatar_string"
+    "build_fixers_map",
+    "capitalize",
+    "fix_avatar_string",
+    "snake_case",
+    "tree"
 ]
